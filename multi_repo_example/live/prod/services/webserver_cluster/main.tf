773db2958a8cc3d9821a4d3c58585fd59230c821
nikolay <nikcbg@yahoo.com>
2019-06-29 16:13:25 +0300

    update tf code to 0.12

--- multi_repo_example/live/prod/services/webserver_cluster/main.tf
+++ multi_repo_example/live/prod/services/webserver_cluster/main.tf
@@ -6,8 +6,8 @@ module "webserver_cluster" {
   source = "git@github.com:nikcbg/TF_Book_Ch_4//module_example/modules/services/webserver_cluster"
 
   cluster_name           = "webservers-prod"
-  db_remote_state_bucket = "${var.db_remote_state_bucket}"
-  db_remote_state_key    = "${var.db_remote_state_key}"
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
 
   instance_type = "m4.large"
   min_size      = 2
@@ -20,7 +20,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   max_size               = 10
   desired_capacity       = 10
   recurrence             = "0 9 * * *"
-  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+  autoscaling_group_name = module.webserver_cluster.asg_name
 }
 
 resource "aws_autoscaling_schedule" "scale_in_at_night" {
@@ -29,5 +29,6 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
   max_size               = 10
   desired_capacity       = 2
   recurrence             = "0 17 * * *"
-  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+  autoscaling_group_name = module.webserver_cluster.asg_name
 }
+
