9b51cc4105b99e943b33e929d97c28270d134b5e
nikolay <nikcbg@yahoo.com>
2019-07-02 15:22:13 +0300

    update tf code to 0.12

--- multi_repo_example/live/stage/services/webserver_cluster/main.tf
+++ multi_repo_example/live/stage/services/webserver_cluster/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "git@github.com:brikis98/terraform-up-and-running-code.git//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.0.2"
+  source = "git@github.com:nikcbg/TF_Book_Ch_4.git//module_example/modules/services/webserver_cluster"
 
   cluster_name           = "webservers-stage"
-  db_remote_state_bucket = "${var.db_remote_state_bucket}"
-  db_remote_state_key    = "${var.db_remote_state_key}"
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
 
   instance_type = "t2.micro"
   min_size      = 2
@@ -16,10 +16,11 @@ module "webserver_cluster" {
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
   type              = "ingress"
-  security_group_id = "${module.webserver_cluster.elb_security_group_id}"
+  security_group_id = module.webserver_cluster.elb_security_group_id
 
   from_port   = 12345
   to_port     = 12345
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
+
